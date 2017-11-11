355a5ccd1f07de62a56f82de2f36d69a9add670f
blue-mica <bluemicam3@gmail.com>
2017-11-11 21:44:11 +0100

    sam teks zadań bez zrobionych

--- dzien3/zadania_domowe.py
+++ dzien3/zadania_domowe.py
@@ -1,41 +1,6 @@
 # 1. sprawdź czy jest wygrana w kółko i krzyżyk
 #   input: string 9 znaków x, o, n
 #   znaki oznaczają pozycje wierszami od gornego
-slowo = str(input("Podaj ciąg 9-ciu znakow X, O lub N \n"))
-# slowo = "OOXONOXXO"
-
-while len(slowo) != 9:
-    print("Podany ciąg nie ma 9 znaków\n")
-    slowo = str(input("Podaj ciąg 9-ciu znakow X, O lub N \n"))
-
-slowo = slowo.upper()
-
-wygrana_X = "XXX"
-wygrana_O = "OOO"
-
-if (wygrana_O in slowo[0:3]) or (wygrana_O in slowo[3:6]) or (wygrana_O in slowo[6:9]):
-    print("Wygrało O ! :)")
-
-elif (wygrana_X in slowo[0:3]) or (wygrana_X in slowo[3:6]) or (wygrana_X in slowo[6:9]):
-    print("Wygrał X ! :)")
-
-elif slowo[0] != "N" and (slowo[0] == slowo[3] and slowo[0] == slowo[6]):
-    print("Wygrana " + slowo[0] + " ! :)")
-
-elif slowo[1] != "N" and (slowo[1] == slowo[4] and slowo[1] == slowo[7]):
-    print("Wygrana " + slowo[1] + " ! :)")
-
-elif slowo[2] != "N" and (slowo[2] == slowo[5] and slowo[2] == slowo[8]):
-    print("Wygrana " + slowo[2] + " ! :)")
-
-elif slowo[0] != "N" and (slowo[0] == slowo[4] and slowo[0] == slowo[8]):
-    print("Wygrana " + slowo[0] + " ! :)")
-
-elif slowo[2] != "N" and (slowo[2] == slowo[4] and slowo[2] == slowo[6]):
-    print("Wygrana " + slowo[2] + " ! :)")
-
-else:
-    print("Nikt nie wygrał. Czyli remis.")
 
 # 2. obliczanie roku przestępnego
 # podzielny przez 4, nie podzielny przez 100 ale podzielny przez 400
