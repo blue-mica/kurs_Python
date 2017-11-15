9ecb242a24d5e5ad2e23934571583e39c6a305b3
blue-mica <bluemicam3@gmail.com>
2017-11-15 18:25:51 +0100

    w domu

--- dzien4/zadania_od_Moni/petle_02.py
+++ dzien4/zadania_od_Moni/petle_02.py
@@ -66,27 +66,18 @@
 
     # wypisz największą liczbę niepodzielną przez 2,3,5,7 ale mniejszą od 1000
 
-zakres = range(1, 1001)
-naj = 0
-for liczba in zakres:
-    if liczba % 2 != 0 and liczba % 3 != 0 and liczba % 5 != 0 and liczba % 7 != 0:
-        if liczba > naj:
-            naj = liczba
+# zakres = range(1, 1001)
+# naj = 0
+# for liczba in zakres:
+#     if liczba % 2 != 0 and liczba % 3 != 0 and liczba % 5 != 0 and liczba % 7 != 0:
+#         if liczba > naj:
+#             naj = liczba
+#
+# print(naj, end=" ")
 
-print(naj, end=" ")
+# zmienna = int(input("Podaj liczbę \n"))
+ile = 5
 
-
-
-
-
-    # sprawdź czy podana jako parametr liczba jest parzysta
-    # sprawdź czy podana jako parametr liczba jest liczbą pierwszą. (podzielna tylko przez 1 i przez samą siebie) (opis tutaj
-    # wylicz największy wspólny dzielnik 2 liczb podanych jako parametry (opis tutaj)
-    # wylicz najmniejszą wspólną wielokrotność liczb podanych jako parametry (opis tutaj)
-    # ile jest liczb pierwszych w zakresie od a do b podanym jako parametry
-    # sprawdź czy 2 liczby podane jako parametr są liczbami względnie pierwszymi. (opis tutaj)
-    # sprawdź czy pesel ( jako parametr) jest prawidłowym peselem (tutaj opis)
-    # oblicz silnię n (n – podane jako parametr) (tutaj opis)
-    # oblicz n-tą liczbę ciągu fibonacciego (n – podane jako parametr) (tutaj opis)
-    # oblicz n-tą liczbę pierwszą (n – podane jako parametr)
-
+while ile >= 1:
+    print("czesc")
+    ile -= 1
