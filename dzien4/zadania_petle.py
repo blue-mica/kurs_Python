ec9446833828d984aa4b752c96e74723204d1ab6
Simon <32460489+blue-mica@users.noreply.github.com>
2017-11-20 17:21:31 +0100

    w domu

---
+++ dzien4/zadania_petle.py
@@ -0,0 +1,27 @@
+# program wydający resztę z dostępnych monet: 5, 2, 1, 0.5, 0.2, 0.1
+
+# Narysuj piramidę Mario - jako input - wysokość piramidy
+# np. piramida wysokości 3 ma wyglądać:
+#
+#   #
+#  ###
+# #####
+#
+
+# program, ktory obliczy ilosc liczb parzystych i nieparzystych w danym zakresie
+
+# program, ktory wypisze liczby od 0 do 20 poza liczbami podzielnymi przez 4
+
+# program, ktory wypisze liczby (0 do 100) z ciagu Fibonacciego
+# 0, 1, 1, 2, 3, 5, 8, 13, 21
+
+# program wypisujący tabliczkę mnozenia (1 do 10) dla podanej liczby
+# uzyc formatowania stringow!
+
+
+# if elif else
+# oblicz wiek psa z ludzkich lat w psich latach
+# przez pierwsze dwa lata, każdy psi rok to 10,5 ludzkiego roku
+# kolejne lata, psi rok to 4 ludzkie lata
+# np. 15 ludzkich lat to 73 psie lata
+
